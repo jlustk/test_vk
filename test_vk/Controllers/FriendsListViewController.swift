15ec8fb17ce7fc6a79b770005cf3d9bc3598c903
Igor <igorlavr@rambler.ru>
2021-03-04 11:52:40 +0200

    3l_LavrynenkoIgor

--- test_vk/Controllers/FriendsListViewController.swift
+++ test_vk/Controllers/FriendsListViewController.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class FriendsListViewController: UITableViewController {
     
-    let friends: [String] = ["Anton", "Vasiia", "Fedor"]
+    var friends: [UserData] = []
+    let ant = UserData(name: "Anton", photo: "image")
+    
+    friends.append(ant)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +26,11 @@ class FriendsListViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FriendsCell", for: indexPath)
+        
+        let myFriend = friends[indexPath.item]
+        
+        cell.textLabel.text = myFriend.name
+        if 
 
         cell.textLabel?.text = friends[indexPath.row]
         return cell
---
+++ test_vk/Models/UserData.swift
@@ -0,0 +1,13 @@
+//
+//  UserData.swift
+//  test_vk
+//
+//  Created by Igor Lavrynenko on 03.03.2021.
+//
+
+import Foundation
+
+struct UserData {
+    var name: String
+    var photo: String?
+}
